597a264645e3deed9a340dc1e4b93d577e9cb816
Ollie Edwards <oliver.edwards@doxy.me>
2024-06-19 23:15:37 +0100

    attempt to make custom api

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,23 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import { get } from "aws-amplify/api";
 
 Amplify.configure(outputs);
+
+async function getItem() {
+  try {
+    const restOperation = get({
+      apiName: "myRestApi",
+      path: "items",
+    });
+    const response = await restOperation.response;
+    console.log("GET call succeeded: ", response);
+  } catch (error) {
+    console.log("GET call failed: ", error);
+  }
+}
+console.log("GET call: ", getItem());
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
