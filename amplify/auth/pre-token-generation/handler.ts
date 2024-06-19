6a7031797feffa1adf8215b649cc5a722cde0dc8
Ollie Edwards <oliver.edwards@doxy.me>
2024-06-19 15:58:01 +0100

    Add custom claims trigger to amplify auth

---
+++ amplify/auth/pre-token-generation/handler.ts
@@ -0,0 +1,39 @@
+import type { PreTokenGenerationV2TriggerHandler } from "aws-lambda";
+
+export const handler: PreTokenGenerationV2TriggerHandler = async (event) => {
+  const userAttributes = event.request.userAttributes;
+
+  const currentMembership = userAttributes["custom:current_membership"];
+  const memberships = userAttributes["custom:memberships"] || "";
+
+  event.response = {
+    claimsAndScopeOverrideDetails: {
+      idTokenGeneration: {
+        claimsToAddOrOverride: {
+          someCustomIdClaim: "wibble",
+        },
+      },
+      accessTokenGeneration: {
+        claimsToAddOrOverride: {
+          current_membership: currentMembership,
+          memberships,
+          jabawocky: "hoof",
+        },
+        scopesToAdd: ["membership:" + currentMembership],
+      },
+    },
+  };
+  console.log(
+    JSON.stringify(
+      {
+        currentMembership,
+        memberships,
+        response: event.response,
+        userAttributes,
+      },
+      null,
+      2
+    )
+  );
+  return event;
+};
